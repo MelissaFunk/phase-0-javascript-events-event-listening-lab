cf13c7c2cd4e428520ca41e24bd39daa3b6ac587
+ <+>
2021-08-16 09:17:43 -0700

    completed assignment + added notes

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function addingEventListener() {
   })
 }
 
+addingEventListener(input);
+
+// could also write this as:
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// -- saves function clickAlert instead of using anonymous funciton so that we can reuse clickAlert for several items in HTML
